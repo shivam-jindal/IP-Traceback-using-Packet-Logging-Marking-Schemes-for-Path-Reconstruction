c603c05d2ba7d988fadd9a4d42e45d13e6c6448e
Shivam <shivamjindal39@gmail.com>
2018-10-23 15:23:18 +0530

    Updates the construct file

--- Source Code/src/iptrace/construct.java
+++ Source Code/src/iptrace/construct.java
@@ -15,6 +15,25 @@ public class Construct extends JPanel implements ActionListener,MouseListener,Mo
 	public JButton done, refresh, browsePacket, sendPacket;
 	public JLabel label2;
 	public JLabel label1;
+	public ImageIcon image,image1, image2;
+	public JButton[] icon_button;
+	public JButton[] lan_icon_button;
+	public JButton attacklan_icon_button, showDegree, showPath;
+	public String msg = "";
+	public static String filename = "";
+	public int mouseX = 0, mouseY = 0, movX = 0, movY = 0;
+	public final int routers, lans;
+	public int flag_button[], router_degree[], parents[], path[];
+	public Point locations[][];
+	float matrix[][];
+	boolean inDrag = false;
+	public Point sender_lan, receiver_lan, attacked_router;
+	boolean isDone = false, isAttacked = false, isPlaced = false, lan1Joined = false, lan2Joined = false, lan0_clicked = false, lan1_clicked = false;
+	boolean isPressed = false;
+	static int count_click = 0, toJoin = 0, row_when_router_clicked, attacked_flag, attacked_button = -1, pathshown = 0, counter = 0;
+	private int row0, rowlast, j1 = 0;
+	Point start,destination;
+	Paint p;
 	int routingTable[][];
 	final int a[];
 	static Graphics j;
@@ -54,7 +73,45 @@ public class Construct extends JPanel implements ActionListener,MouseListener,Mo
 		label1.setBounds(375, 20, 700, 28);
 		panel.add(label1);
 		done = new JButton("DONE");
+		done.addActionListener(this);
+		done.setLocation(170, 680);
+		done.setSize(new Dimension(100, 30));
+		panel.add(done);
+		image2 = new ImageIcon("/home/shivam/sem-7/psk-project/Source Code/IP-Traceback-using-Packet-Logging-Marking-Schemes-for-Path-Reconstruction/Source Code/src/iptrace/assets/refresh1.png");
+		refresh = new JButton(image2);
+		refresh.addActionListener(this);
+		refresh.setLocation(280, 680);
+		refresh.setSize(new Dimension(30, 30));
+		refresh.setEnabled(false);
+		panel.add(refresh);
+		showDegree = new JButton("Show Degree");
+		showDegree.addActionListener(this);
+		showDegree.setLocation(320, 680);
+		showDegree.setSize(new Dimension(200, 30));
+		showDegree.setEnabled(false);
+		panel.add(showDegree);
+		showPath = new JButton("Show Shortest Path");
+		showPath.addActionListener(this);
+		showPath.setLocation(530, 680);
+		showPath.setSize(new Dimension(250, 30));
+		showPath.setEnabled(false);
+		panel.add(showPath);
+		browsePacket = new JButton("Browse Packet");
+		browsePacket.addActionListener(this);
+		browsePacket.setLocation(790, 680);
+		browsePacket.setSize(new Dimension(250, 30));
+		browsePacket.setEnabled(false);
+		panel.add(browsePacket);
+		sendPacket = new JButton("Send Packet");
+		sendPacket.addActionListener(this);
+		sendPacket.setLocation(1050, 680);
+		sendPacket.setSize(new Dimension(200, 30));
+		sendPacket.setEnabled(false);
+		panel.add(sendPacket);
 
+		image = new ImageIcon("/home/shivam/sem-7/psk-project/Source Code/IP-Traceback-using-Packet-Logging-Marking-Schemes-for-Path-Reconstruction/Source Code/src/iptrace/assets/router1.png");
+		image1 = new ImageIcon("/home/shivam/sem-7/psk-project/Source Code/IP-Traceback-using-Packet-Logging-Marking-Schemes-for-Path-Reconstruction/Source Code/src/iptrace/assets/pc1.png");
+		lan_icon_button = new JButton[2];
 		icon_button = new JButton[routers];
 		for(int i = 0; i < routers; i++) {
 			icon_button[i] = new JButton(image);
@@ -87,5 +144,98 @@ public class Construct extends JPanel implements ActionListener,MouseListener,Mo
 		jpm = new JPopupMenu();
 		jmi = new JMenuItem("Add");
 		jpm.add(jmi);
+		for( int i = 0; i<routers;i++){
+			if(i == 0){
+				icon_button[0].addMouseListener(new MouseAdapter(){
+					public void mouseClicked(MouseEvent me) {
+						if(SwingUtilities.isRightMouseButton(me)){
+							jpm.show(icon_button[0], me.getX(),me.getY());
+						}
+					}
+				});
+			}
+			if(i == 1){
+				icon_button[1].addMouseListener(new MouseAdapter(){
+					public void mouseClicked(MouseEvent me) {
+						if(SwingUtilities.isRightMouseButton(me)){
+							jpm.show(icon_button[1], me.getX(),me.getY());
+						}
+					}
+				});
+			}
+			if(i == 2){
+				icon_button[2].addMouseListener(new MouseAdapter(){
+					public void mouseClicked(MouseEvent me) {
+						if(SwingUtilities.isRightMouseButton(me)){
+							jpm.show(icon_button[2], me.getX(),me.getY());
+						}
+					}
+				});
+			}
+			if(i == 3){
+				icon_button[3].addMouseListener(new MouseAdapter(){
+					public void mouseClicked(MouseEvent me) {
+						if(SwingUtilities.isRightMouseButton(me)){
+							jpm.show(icon_button[3], me.getX(),me.getY());
+						}
+					}
+				});
+			}
+			if(i == 4){
+				icon_button[4].addMouseListener(new MouseAdapter(){
+					public void mouseClicked(MouseEvent me) {
+						if(SwingUtilities.isRightMouseButton(me)){
+							jpm.show(icon_button[4], me.getX(),me.getY());
+						}
+					}
+				});
+			}
+			if(i == 5){
+				icon_button[5].addMouseListener(new MouseAdapter(){
+					public void mouseClicked(MouseEvent me) {
+						if(SwingUtilities.isRightMouseButton(me)){
+							jpm.show(icon_button[5], me.getX(),me.getY());
+						}
+					}
+				});
+			}
+			if(i == 6){
+				icon_button[6].addMouseListener(new MouseAdapter(){
+					public void mouseClicked(MouseEvent me) {
+						if(SwingUtilities.isRightMouseButton(me)){
+							jpm.show(icon_button[6], me.getX(),me.getY());
+						}
+					}
+				});
+			}
+			if(i == 7){
+				icon_button[7].addMouseListener(new MouseAdapter(){
+					public void mouseClicked(MouseEvent me) {
+						if(SwingUtilities.isRightMouseButton(me)){
+							jpm.show(icon_button[7], me.getX(),me.getY());
+						}
+					}
+				});
+			}
+			if(i == 8){
+				icon_button[8].addMouseListener(new MouseAdapter(){
+					public void mouseClicked(MouseEvent me) {
+						if(SwingUtilities.isRightMouseButton(me)){
+							jpm.show(icon_button[8], me.getX(),me.getY());
+						}
+					}
+				});
+			}
+			if(i == 9){
+				icon_button[9].addMouseListener(new MouseAdapter(){
+					public void mouseClicked(MouseEvent me) {
+						if(SwingUtilities.isRightMouseButton(me)){
+							jpm.show(icon_button[9], me.getX(),me.getY());
+						}
+					}
+				});
+			}
+		}
+	}
 
 }
