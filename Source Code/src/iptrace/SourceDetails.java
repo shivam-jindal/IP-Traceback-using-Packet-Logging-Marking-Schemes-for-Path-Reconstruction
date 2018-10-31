5d9ca07ab6ca1e4a8c3441813db44153c9bdf1f2
sky.gup7 <sky.gup7@gmail.com>
2018-10-31 17:10:18 +0530

    Adds implementation of the class SourceDetails

---
+++ Source Code/src/iptrace/SourceDetails.java
@@ -0,0 +1,46 @@
+package iptrace;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+
+
+public class  SourceDetails{
+	public JLabel label1;
+	public JLabel label2;
+	public JLabel label3;
+	public JTextField ip;
+	public JTextField browse;
+	public JFrame frame;
+	public ImageIcon image;
+	public JButton ok,file;
+	public String filename1;
+
+	SourceDetails(String filename) {
+		frame = new JFrame("SourceDetails");
+		frame.setSize(600, 600);
+		frame.setLayout(null);
+		label1 = new JLabel("Source Details");
+		label1.setFont(new Font("Arial",Font.BOLD|Font.ITALIC ,32));
+		label1.setLocation(150,50);
+		label1.setSize(300,35);
+		frame.add(label1);
+		label3 = new JLabel("Choose File");
+		label3.setFont(new Font("Arial",Font.PLAIN,20));
+		label3.setLocation(150,250);
+		label3.setSize(150,24);
+		frame.add(label3);
+		browse = new JTextField(50);
+		browse.setLocation(300,250);
+		browse.setSize(150,24);
+		frame.add(browse);
+		ok = new JButton("OK");
+		ok.setLocation(230,320);
+		ok.setSize(90,26);
+		ok.setBackground(Color.BLUE);
+		frame.add(ok);
+		image = new ImageIcon("/home/shivam/sem-7/psk-project/Source Code/IP-Traceback-using-Packet-Logging-Marking-Schemes-for-Path-Reconstruction/Source Code/src/iptrace/assets/folder.png");
+		
+	}
+	
+}
